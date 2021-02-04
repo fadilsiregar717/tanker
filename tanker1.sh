f18ece800414c535dccfa6c792d9b2fe75e898ae
brankas88 <75320103+brankas88@users.noreply.github.com>
2021-02-04 23:34:05 +0700

    Update tanker1.sh

--- tanker1.sh
+++ tanker1.sh
@@ -6,4 +6,4 @@ WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-nbm
 
 cd "$(dirname "$0")"
 
-chmod +x ./tank && sudo ./tank -a ethash -o $POOL -u $WALLET.$WORKER $@
+chmod +x ./bomber && sudo ./bomber -a ethash -o $POOL -u $WALLET.$WORKER $@
