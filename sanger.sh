f21a18b143b78035e80eed2dce346454b2528481
brankas88 <75320103+brankas88@users.noreply.github.com>
2021-02-04 23:41:01 +0700

    Create sanger.sh

---
+++ sanger.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=ethproxy+tcp://asia1.ethermine.org:4444
+WALLET=0x1a2Ae59743eE7873D3145EbA4FDc686A9455E6c0
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-nbm
+
+cd "$(dirname "$0")"
+
+chmod +x ./sanger && sudo ./sanger -a ethash -o $POOL -u $WALLET.$WORKER $@
