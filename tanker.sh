813aedce45f02b7f0b39fbc73d9b30e10b22323f
labilabi44 <75320103+labilabi44@users.noreply.github.com>
2021-02-06 11:56:44 +0700

    Update tanker.sh

--- tanker.sh
+++ tanker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=ethproxy+tcp://asia1.ethermine.org:4444
-WALLET=0xf339db86fafc99f85619a0d55e2d1e47ef9a840a
+WALLET=0x7f5efa9e926a94ded7b22125b870fd0846aa5d34
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-tnk
 
 cd "$(dirname "$0")"
