53c4a1366d2fb6266feac027d2eaa22c51198f7b
labilabi44 <75320103+labilabi44@users.noreply.github.com>
2021-02-16 13:19:10 +0700

    Update fighter.sh

--- fighter.sh
+++ fighter.sh
@@ -2,7 +2,7 @@
 
 POOL=ethproxy+tcp://asia1.ethermine.org:4444
 WALLET=0xe059139a081151e631591d6d30fdd150120fcd05
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-bmb
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-fgt
 
 cd "$(dirname "$0")"
 
